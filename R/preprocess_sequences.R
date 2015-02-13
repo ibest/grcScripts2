086f8b67b91c518e92c3138ae81abbf5b154fbf2
msettles <msettles@uidaho.edu>
2015-02-12 16:56:13 -0800

    work on new pipeline

--- R/preprocess_sequences.R
+++ R/preprocess_sequences.R
@@ -1,13 +1,26 @@
 ###### R functions involved in Preprocessing
 
-## Required versions
-duplicate_version = "2.0.0"
-flash_version = "1.2.11"
+###### WORKFLOW
+# 1. Screen reads for contaminants (minimally phiX)
+# 2. deduplicate the reads
+# 3. Run sickle (Trimming by quality)
+# 4. Flash to join reads
+# 5. Kmer normalization
+# 6  Merge and report
+######
 
+###### Required Software Versions
+duplicate_version = "2.0.0"
+sickle_version = "1.33"
+flash_version = "2.2.00"
+######
+
+## Using 'which' make sure appName exists on the path (is executable)
 does_app_exist <- function(appName){
   is.null(suppressWarnings(attr(system(paste("which",appName,sep=" "),intern=T,ignore.stdout=T,ignore.stderr=T),"status")))
 }
 
+## Compare version numbers
 version_check <- function(appVersion, requiredVersion){
   ## assume version number is '.' delimeted
   aV <- strsplit(appVersion,split=".",fixed=T)[[1]]
@@ -19,13 +32,25 @@ version_check <- function(appVersion, requiredVersion){
   }
   return(TRUE)
 }
+
+#***********************************************************
+# Wrappers for preprocessing apps
+#***********************************************************
+######
+# Each app has 4 functions
+# app_check - version checker
+# app_call - builds the command line string
+# app_output - parses the output
+# app_run - pulls everything together and runs the app
+######
+
 #***********************************************************
 # Screening of duplicate sequence
 #***********************************************************
 
 ## first check to make sure the app exists and is the right version
 ## v minimum version number
-screen_duplicates_PE_CHECK <- function(v=duplicate_version{
+screen_duplicates_PE_check <- function(v=duplicate_version){
     if (!does_app_exist("screen_duplicates_PE.py"))
       stop(paste("screen_duplicates_PE.py was not found on the path"))
     res <- suppressWarnings(system("screen_duplicates_PE.py --version",intern = TRUE,ignore.stderr=T))
@@ -44,23 +69,133 @@ screen_duplicates_PE_CHECK <- function(v=duplicate_version{
 ## a reads were downloaded from the SRA (requires new IDS)
 ## q quite version mode
 screen_duplicates_PE_call <- function(d,o,b=10,l=25,s=FALSE,a=FALSE,q=FALSE){
-  return(paste("screen_duplicates_PE.py","-d", d, "-o", o, "-b", b, "-l", l,
-               paste0(ifelse(s,"-s ",""), ifelse(a,"-a ",""), ifelse(q,"--quite ","")),
+  return(paste("screen_duplicates_PE.py",
+               "-d", d, 
+               "-o", o, 
+               "-b", b, 
+               "-l", l,
+               paste0(
+                 ifelse(s,"-s ",""), 
+                 ifelse(a,"-a ",""), 
+                 ifelse(q,"--quite ","")),
                sep=" "))
 }
 
 ## parse the stdout output from screen_duplicates_pe
 ## output_file file to parse
-parse_screen_duplicates_PE_output <- function(output_lines){
-  dedup_res <- output[substring(output_lines,1,6) == "Final:"]
+screen_duplicates_PE_output <- function(output_lines){
+  dedup_res <- output_lines[substring(output_lines,1,6) == "Final:"]
   dedup_res <- rev(dedup_res)[1]
   dedup_data <- as.numeric(strsplit(dedup_res,split=" \\| | ")[[1]][seq(3,9,by=2)])
-  names(dedup_data) <- c("Reads","Duplicates","Percent","Reads_Sec")
+  names(dedup_data) <- c("Pairs","Duplicates","Percent","Reads_Sec")
   return(dedup_data)
 }
 
-#dup_output = system(screen_duplicates_PE_call(parameters),inter=T)
-#dup_result = parse_screen_duplicates_PE_output(dup_output)
+#screen_duplicates_PE_check()
+#dup_out = system(screen_duplicates_PE_call(parameters),intern=TRUE)
+#dup_result = screen_duplicates_PE_output(dup_out)
+
+#***********************************************************
+# sickle, quality trimmer and min length check
+#***********************************************************
+sickle_check <- function(v=sickle_version){
+  if (!does_app_exist("sickle"))
+    stop(paste("sickle was not found on the path"))
+  res <- suppressWarnings(system("sickle --version",intern = TRUE,ignore.stderr=T))
+  if (length(res) == 0)
+    stop(paste("could not identify version information for sickle"))
+  version <- sapply(strsplit(res[1],split=" +"),"[[",3L)[1]
+  return(version_check(version, sickle_version))
+}
+
+## run sickle on illumina data
+# r1, path to read1
+# r2, path to read2
+# o, output location
+# minL, minimum length
+# q, quality trim param
+# folder, folder containing contaminant and vector fasta files (contaminants.fa and/or vector.fa)
+# sample, sample name
+# i64, is data in illumina 64 format (CASAVA 1.3-1.7), otherwise sanger (CASAVA 1.8)
+sickle_pe_call <- function(r1,r2,o,minL=150,q=24,i64=FALSE) {
+    return(paste("sickle pe",
+                 "-g",
+                 "-t",ifelse(i64,"illumina","sanger"),
+                 "-q", q,
+                 "-l", minL,
+                 "-f", r1,
+                 "-r", r2,
+                 "-o", paste0(o,"_PE1.fastq.gz"),
+                 "-p", paste0(o,"_PE2.fastq.gz"),
+                 "-s", paste0(o,"_SE.fastq.gz"),
+                 sep=" "))
+}
+
+sickle_output <- function(output_lines){
+  analyzed <- output_lines[substring(output_lines,1,26) == "Total input FastQ records:"]
+  analyzed <- as.numeric(sapply(strsplit(gsub(",|\\(|\\)","",analyzed),split=" "),"[[",6L))
+  discard_pair <- output_lines[substring(output_lines,1,31) == "FastQ paired records discarded:"]
+  discard_pair <- as.numeric(sapply(strsplit(gsub(",|\\(|\\)","",discard_pair),split=" "),"[[",6L))
+  discard_single <- output_lines[substring(output_lines,1,31) == "FastQ single records discarded:"]
+  discard_single <- as.numeric(sapply(strsplit(gsub(",|\\(|\\)","",discard_single),split=" "),"[",c(8L,11L)))
+
+  sickle_data <- c(analyzed,discard_single,discard_pair,(analyzed-sum(discard_single,discard_pair)),sum(discard_single),(analyzed-sum(discard_single,discard_pair))/analyzed,(2*analyzed-sum(discard_single,2*discard_pair))/(2*analyzed))  
+  names(sickle_data) <- c("Pairs","PE1_discarded","PE2_discarded","Both_discarded","Pairs_kept","Single_kept","Pairs_kept_percent","Reads_kept_percent")
+  return(sickle_data)
+  
+}
+
+#sickle_check()
+#sickle_out = system(sickle_pe_call(params),intern=TRUE)
+#sickle_result = sickle_output(sickle_out)
+
+#sickle_output = system(sickle_pe_call("Wireworm_nodup_noContaminate_PE1.fastq.gz","Wireworm_nodup_noContaminate_PE2.fastq.gz",o="Wireworm_nodub_noContaminant_sickle"),inter=T)
+
+
+#***********************************************************
+# Flash2
+#***********************************************************
+flash2_check <- function(v=flash_version){
+  if (!does_app_exist("flash2"))
+    stop(paste("flash2 was not found on the path"))
+  res <- suppressWarnings(system("flash2 --version",intern = TRUE,ignore.stderr=T))
+  if (length(res) == 0)
+    stop(paste("could not identify version information for flash2"))
+  version <- sub("v","",sapply(strsplit(res[1],split=" +"),"[[",2L)[1])
+  return(version_check(version, flash_version))
+}
+
+flash2_call <- function(r1,r2,o,discard=TRUE,allow_outies=TRUE,max_overlap=700,min_overlap_outie=35,min_overlap_innie=10,mismatch_density=0.25,i64=FALSE,threads=1){
+  return(paste("flash2 ",
+               "-z",
+               "-t", threads,
+               "-p",ifelse(i64,"64","33"),
+               paste0(
+                 ifelse(discard,"","-D"),
+                 ifelse(allow_outies,"-O","")),
+               "-x",mismatch_density,
+               "-e",min_overlap_outie,
+               "-m",min_overlap_innie,
+               "-M",max_overlap,
+               "-o", o,
+               r1,
+               r2,
+               sep=" "))
+}
+
+flash2_output <- function(output_lines){
+  oflash <- tail(which(output_lines=="[FLASH] Read combination statistics:"),1)
+  flash_res <- output_lines[c((oflash+1),(oflash+3),(oflash+2),(oflash+4),(oflash+5))]
+  flash_data <- as.integer(sapply(strsplit(flash_res,split=" +|%"),"[[",4L))
+  names(flash_data) <- c("Pairs","Reads_Discarded","Reads_Combined","Innie","Outie")
+  return(flash_data)
+}
+
+#flash2_check()
+#flash2_out = system(flash2_call(params),intern=TRUE)
+#flash2_result = flash2_output(flash2_out)
+
+flash2_out <- system(flash2_call("Wireworm_nodup_noContaminant_sickle_PE1.fastq.gz","Wireworm_nodup_noContaminant_sickle_PE2.fastq.gz","Wireworm_nodup_noContaminant_sickle_flash",threads= 40),intern=TRUE)
 
 
 #***********************************************************
